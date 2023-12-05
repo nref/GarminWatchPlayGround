06115d202f55fe43cae57d32a997f753b6d3ecf4
Doug Slater <doug@slater.dev>
2023-12-04 20:06:56 -0500

    WIP add Windows support (aka migrate from bluez-dotnet to 32feet)

---
+++ src/Ble.Windows/WindowsBleDevice.cs
@@ -0,0 +1,146 @@
+﻿using Ble.Interfaces;
+using InTheHand.Bluetooth;
+using Linux.Bluetooth;
+
+namespace Ble.Windows;
+
+public class WindowsBleDevice : IBleDevice
+{
+  private readonly BluetoothDevice _device;
+
+  public WindowsBleDevice(BluetoothDevice device)
+  {
+    _device = device;
+  }
+
+  public Task CancelPairingAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task ConnectAsync()
+  {
+    await _device.Gatt.ConnectAsync();
+  }
+
+  public Task ConnectProfileAsync(string UUID)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task DisconnectAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task DisconnectProfileAsync(string UUID)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<T> GetAsync<T>(string prop)
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task<IGattService> GetServiceAsync(string serviceUUID)
+  {
+    GattService service = await _device.Gatt.GetPrimaryServiceAsync(BluetoothUuid.FromGuid(Guid.Parse(serviceUUID)));
+    return new WindowsBleService(service);
+  }
+
+  public Task PairAsync()
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task SetAsync(string prop, object val)
+  {
+    throw new NotImplementedException();
+  }
+}
+
+public class WindowsBleService : IGattService
+{
+  private readonly GattService _gatt;
+
+  public WindowsBleService(GattService gatt)
+  {
+    _gatt = gatt;
+  }
+
+  public Task<T> GetAsync<T>(string prop)
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task<IGattCharacteristic> GetCharacteristicAsync(string characteristicUUID)
+  {
+    InTheHand.Bluetooth.GattCharacteristic gc = await _gatt.GetCharacteristicAsync(BluetoothUuid.FromGuid(Guid.Parse(characteristicUUID)));
+    return new WindowsGattCharacteristic(gc);
+  }
+
+  public Task SetAsync(string prop, object val)
+  {
+    throw new NotImplementedException();
+  }
+}
+
+public class WindowsGattCharacteristic : IGattCharacteristic
+{
+  private readonly InTheHand.Bluetooth.GattCharacteristic _gattChar;
+
+  public event Interfaces.GattCharacteristicEventHandlerAsync Value;
+
+  public WindowsGattCharacteristic(InTheHand.Bluetooth.GattCharacteristic gattChar)
+  {
+    _gattChar = gattChar;
+
+    _gattChar.CharacteristicValueChanged += HandleCharacteristicValueChanged;
+  }
+
+  private async void HandleCharacteristicValueChanged(object? sender, GattCharacteristicValueChangedEventArgs e)
+  {
+    await OnValueIn(_gattChar, e);
+  }
+
+  public Task<string> GetUUIDAsync()
+  {
+    return Task.FromResult(_gattChar.Uuid.ToString());
+  }
+
+  public async Task<byte[]> ReadValueAsync(IDictionary<string, object> Options)
+  {
+    return await _gattChar.ReadValueAsync();
+  }
+
+  public async Task WriteValueAsync(byte[] Value, IDictionary<string, object> Options)
+  {
+    bool withResponse = Options.TryGetValue("type", out var value) && value is string type && type == "request";
+    if (withResponse) 
+    {
+      await _gattChar.WriteValueWithResponseAsync(Value);
+    }
+    else
+    {
+      await _gattChar.WriteValueWithoutResponseAsync(Value);
+    }
+  }
+
+  private Task? OnValueIn(InTheHand.Bluetooth.GattCharacteristic sender, GattCharacteristicValueChangedEventArgs e)
+  {
+    return Value?.Invoke(this, new WindowsGattCharacteristicArgs(e));
+  }
+
+}
+internal class WindowsGattCharacteristicArgs : IGattCharacteristicValueEventArgs
+{
+    private readonly GattCharacteristicValueChangedEventArgs _gattCharacteristicValueEventArgsImplementation;
+
+    public WindowsGattCharacteristicArgs(GattCharacteristicValueChangedEventArgs gattCharacteristicValueEventArgs)
+    {
+        _gattCharacteristicValueEventArgsImplementation = gattCharacteristicValueEventArgs;
+    }
+
+    public byte[] Value => _gattCharacteristicValueEventArgsImplementation.Value;
+}
--- src/GarminWatchTools/Program.cs
+++ src/GarminWatchTools/Program.cs
@@ -1,39 +1,84 @@
-﻿using Ble.Linux;
+﻿using Ble.Interfaces;
+//using Ble.Linux;
+using Ble.Windows;
 using Garmin.Ble.Lib;
 using Garmin.Ble.Lib.Devices.Garmin;
-using vestervang.DotNetBlueZ;
+using InTheHand.Bluetooth;
+//using vestervang.DotNetBlueZ;
 
-const string MyWatchMac = "E6:EA:17:A8:92:E0";
+const string MyWatchMac = "90:F1:57:93:83:85";
 var deviceAddress = MyWatchMac;
 var timeout = TimeSpan.FromSeconds(15);
 
 AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) => Console.Error.WriteLine(eventArgs.ExceptionObject);
 
-var adapters = await BlueZManager.GetAdaptersAsync();
-if (adapters.Count == 0)
+bool isLinux = OperatingSystem.IsLinux();
+bool isWindows = OperatingSystem.IsWindows();
+
+IBleDevice? bleDevice = null;
+if (isLinux)
 {
-    throw new Exception("No Bluetooth adapters found.");
+  //var adapters = await BlueZManager.GetAdaptersAsync();
+  //if (adapters.Count == 0)
+  //{
+  //  throw new Exception("No Bluetooth adapters found.");
+  //}
+
+  //var adapter = adapters.First();
+
+  //if (!await adapter.GetPoweredAsync())
+  //  await adapter.SetPoweredAsync(true);
+
+  //var device = await adapter.GetDeviceAsync(deviceAddress);
+  //if (device == null)
+  //{
+  //  Console.WriteLine(
+  //      $"Bluetooth peripheral with address '{deviceAddress}' not found. Use `bluetoothctl` or Bluetooth Manager to scan and possibly pair first.");
+  //  return;
+  //}
+
+  //Console.WriteLine("Connecting...");
+  //await device.ConnectAsync();
+  //await device.WaitForPropertyValueAsync("Connected", value: true, timeout);
+  //Console.WriteLine($"Connected to {await device.GetNameAsync()}.");
+
+  //await device.WaitForPropertyValueAsync("ServicesResolved", value: true, timeout);
+
+  //bleDevice = new LinuxBleDevice(device);
+}
+else if (isWindows)
+{
+  //var scan = await Bluetooth.RequestLEScanAsync();
+
+  var filter = new BluetoothLEScanFilter();
+  filter.Services.Add(GarminConstants.UUID_SERVICE_GARMIN_3);
+  RequestDeviceOptions options = new()
+  {
+    //AcceptAllDevices = true,
+  };
+  options.Filters.Add(filter);
+  BluetoothDevice device = await Bluetooth.RequestDeviceAsync(options);
+
+  if (device == null) { return; }
+
+  await device.Gatt.ConnectAsync();
+
+  if (!device.Gatt.IsConnected) { return; }
+  device.GattServerDisconnected += HandleDisconnected;
+
+  bleDevice = new WindowsBleDevice(device);
+  List<GattService> services = await device.Gatt.GetPrimaryServicesAsync();
+}
+else
+{
+  return;
 }
 
-var adapter = adapters.First();
-
-if (!await adapter.GetPoweredAsync())
-    await adapter.SetPoweredAsync(true);
-
-var device = await adapter.GetDeviceAsync(deviceAddress);
-if (device == null)
+async void HandleDisconnected(object? sender, EventArgs e)
 {
-    Console.WriteLine(
-        $"Bluetooth peripheral with address '{deviceAddress}' not found. Use `bluetoothctl` or Bluetooth Manager to scan and possibly pair first.");
-    return;
+  if (sender is not BluetoothDevice bd) { return; }
+  await bd.Gatt.ConnectAsync();
 }
-
-Console.WriteLine("Connecting...");
-await device.ConnectAsync();
-await device.WaitForPropertyValueAsync("Connected", value: true, timeout);
-Console.WriteLine($"Connected to {await device.GetNameAsync()}.");
-
-await device.WaitForPropertyValueAsync("ServicesResolved", value: true, timeout);
 
 var logger = new ConsoleLogger();
 logger.DisableDebugInZone(DebugZones.PackageSend);
@@ -45,15 +90,16 @@ logger.DisableDebugInZone(DebugZones.MessageSend);
 logger.DisableDebugInZone(DebugZones.MessageSendBytes);
 logger.DisableDebugInZone(DebugZones.FileTransferProgress);
 var swim2 = new Swim2Device(logger);
-Task.Run(async () => await swim2.DownloadAGpsData());
-await swim2.Init(new LinuxBleDevice(device));
+_ = Task.Run(swim2.DownloadAGpsData);
 
-Console.WriteLine("Press Enter to read file list:");
-Console.ReadLine();
+await swim2.Init(bleDevice);
+
+//Console.WriteLine("Press Enter to read file list:");
+//Console.ReadLine();
 // await swim2.DownloadGarminXml();
 // await swim2.DownloadFile(5);
 
- swim2.DownloadAllActivities();
+ //swim2.DownloadAllActivities();
 
 while (true)
     await Task.Delay(1000);
